2b75aad9f0ae951b430e6feb679068c0b1ef9209
Kuber Kalra <kuber_kalra@ymail.com>
2020-05-27 15:24:21 +0100

    Cleanup in Initial.js; Identified memory leak after logging in due to unhandled promise or unmounted components

--- HavenApp/screens/auth/Initial.js
+++ HavenApp/screens/auth/Initial.js
@@ -1,24 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { View } from 'react-native';
-import { AppLoading } from 'expo';
-import * as Icon from '@expo/vector-icons';
 import { withFirebaseHOC } from '../../config/';
 
 const Initial = (props) => {
-  const [isAssetsLoadingComplete, setIsAssetsLoadingComplete] = useState(false);
-
   useEffect(() => {
-    try {
-      props.firebase.checkUserAuth((user) => {
-        if (user) {
-          props.navigation.navigate('App');
-        } else {
-          props.navigation.navigate('Auth');
-        }
-      });
-    } catch (error) {
-      console.log(error);
-    }
+    props.firebase.checkUserAuth((user) => {
+      if (user) {
+        props.navigation.navigate('App');
+      } else {
+        props.navigation.navigate('Auth');
+      }
+    });
   });
 
   return <View></View>;
