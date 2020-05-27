8833fab2ac33a4c73bbc7467bcea4140fa3efdec
Kuber Kalra <kuber_kalra@ymail.com>
2020-05-27 22:36:39 +0100

    Added

--- HavenApp/navigation/AppNavigator.js
+++ HavenApp/navigation/AppNavigator.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { createStackNavigator } from 'react-navigation-stack';
-import { HomeScreen, MeditationTimer } from '../screens/';
+import { HomeScreen, MeditationTimer, Monologues } from '../screens/';
 
 const AppNavigator = createStackNavigator(
   {
     Home: HomeScreen,
     MeditationTimer: MeditationTimer,
+    Monologues: Monologues
   },
   {
     initialRouteName: 'Home',
--- HavenApp/screens/index.js
+++ HavenApp/screens/index.js
@@ -4,6 +4,7 @@ export HomeScreen from './HomeScreen';
 export Login from './auth/Login';
 export Signup from './auth/Signup';
 export ForgotPassword from './auth/ForgotPassword';
+export Monologues from './Monologues';
 import Initial from './auth/Initial';
 
 export default Initial;
