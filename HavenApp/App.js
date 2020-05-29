b5ae563980c638e40a377925df9e80d1a53610c4
Kuber Kalra <kuber_kalra@ymail.com>
2020-05-29 20:34:33 +0100

    Added persistence to redux to store the files locally

--- HavenApp/App.js
+++ HavenApp/App.js
@@ -1,22 +1,34 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, AsyncStorage } from 'react-native';
 import * as Font from 'expo-font';
 import Firebase, { FirebaseProvider } from './config/';
 import AppContainer from './navigation';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import audioReducer from './store/reducer';
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
 
 const reducer = combineReducers({
   audios: audioReducer,
 });
-const store = createStore(reducer);
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+const store = createStore(persistedReducer);
+const persistor = persistStore(store);
 
 export default function App() {
   return (
     <FirebaseProvider value={Firebase}>
       <Provider store={store}>
-        <AppContainer />
+        <PersistGate persistor={persistor}>
+          <AppContainer />
+        </PersistGate>
       </Provider>
     </FirebaseProvider>
   );
